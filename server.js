d20cd02e93e71aa46f471e223ce60a259caba828
Wilmer <wilmerh701@gmail.com>
2020-12-02 22:32:54 -0500

    Cleaned Errors

--- server.js
+++ server.js
@@ -6,14 +6,16 @@ const app = express();
 const http = require('http').Server(app);
 
 //socket io
-const socketio = require('socket.io')(http);
+const io = require('socket.io')(http);
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-    res.render('index.html');
+    res.render('public/index.html');
 });
 
 // Run when client connects
-io.socket.on('connection', function(socket) {
+io.sockets.on('connection', function(socket) {
   console.log("User Is Connected");
   
   socket.on('username', function(username) {
@@ -23,10 +25,10 @@ io.socket.on('connection', function(socket) {
   
   socket.on('disconnect', function(username) {
     socket.username = username;
-    io.emit('is_online', '😿 <i>' + socket.username + ' joined the chat..</i>');
+    io.emit('is_online', '😿 <i>' + socket.username + ' left the chat..</i>');
   });
   
-  socket.on('disconnect', function(username) {
+  socket.on('chat_message', function(message) {
     io.emit('chat_message', '<strong>' + socket.username + '</strong>: ' + message);
   });
   
