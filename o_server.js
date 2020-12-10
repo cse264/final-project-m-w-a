b05dbd9106ad7544c7dc68f16cb6f54d45c694d0
Wilmer <wilmerh701@gmail.com>
2020-12-09 22:02:50 -0500

    Styling Form

--- o_server.js
+++ o_server.js
@@ -7,8 +7,13 @@ const server = http.createServer(app);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+<<<<<<< HEAD
 app.listen(80, function() {
 	console.log('Server running on port 80');
+=======
+app.listen(process.env.PORT || 80, function() {
+	console.log('Server running on port 3000');
+>>>>>>> 723cbc41fd34084b0b4dac9f44558b7bcabf348b
 });
 
 
